bbef37d15d1800e85fce98918728e18b6ad89c6c
Farhan <ullah8113@gmail.com>
2025-05-03 19:13:32 +0600

    auth Api added

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (
+          credentials.username === "admin" &&
+          credentials.password === "admin123"
+        ) {
+          return { id: "admin", name: "Admin User" };
+        }
+        return null;
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
